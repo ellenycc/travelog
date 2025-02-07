ed9af9211c47f9657d8da017414e8c80174038db
Ellen CHan <ellenycc@gmail.com>
2025-02-07 08:13:50 +0000

    changed settings

--- django_site/settingsprod.py
+++ django_site/settingsprod.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['138.68.128.95/']
+ALLOWED_HOSTS = ['138.68.128.95']
 
 
 # Application definition
