d7ff5ee4e305771e0823c9e8aaea7b2281fc65c1
Ellen CHan <ellenycc@gmail.com>
2025-02-12 19:54:45 +0000

    update allowed hosts

--- django_site/settings.py
+++ django_site/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*.ondigitalocean.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-blog-3a3v3.ondigitalocean.app',
+                 'localhost', '127.0.0.1']
 
 
 # Application definition
