0918aa4044edb9b3c212ccfb1d2486ef2bc55b35
Ellen CHan <ellenycc@gmail.com>
2025-02-12 08:12:58 +0000

    change static settings

--- django_site/settings.py
+++ django_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*.ondigitalocean.app', 'blog-ellen.site']
 
 
 # Application definition
@@ -137,7 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
