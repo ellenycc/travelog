28e8888f74dc1554a83a14f7b5fb33ee7325cabb
Ellen CHan <ellenycc@gmail.com>
2025-03-12 07:57:12 +0000

    Fix background image

--- django_site/settings.py
+++ django_site/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
 DEBUG = False
 
 ALLOWED_HOSTS = ['django-blog-3a3v3.ondigitalocean.app',
