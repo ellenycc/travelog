e6bf7b44ffaca65cd8a21e7d79d79fdc2db76169
Ellen CHan <ellenycc@gmail.com>
2025-07-12 09:28:49 +0100

    add domain to allowed host in settings

--- django_site/settings.py
+++ django_site/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-blog-3a3v3.ondigitalocean.app',
-                 'localhost', '127.0.0.1', '167.172.61.162']
+ALLOWED_HOSTS = ['blog-ellen.site', 'localhost', '127.0.0.1', '167.172.61.162']
 
 
 # Application definition
